3af78bf7034ba47809302b4921e390e0a7d0a8cc
GitHub Action <action@github.com>
2020-09-12 07:13:56 +0000

    Publish build efc62597c625d677ac6068cc4b1207ae89956ba3

---
+++ docs/precache-manifest.2868381d0add765dff6d3a0d7be8b5a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dc056ffcb72fe32a99f34a57a90201",
+    "url": "https://rdok.github.io/lean-pdf-editor/index.html"
+  },
+  {
+    "revision": "311139dc363f5bfffa57",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/css/2.59815e44.chunk.css"
+  },
+  {
+    "revision": "96e1b4f39e37e059a10f",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/css/main.720d81d7.chunk.css"
+  },
+  {
+    "revision": "311139dc363f5bfffa57",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/js/2.e0f13d00.chunk.js"
+  },
+  {
+    "revision": "697e367ae7c6281023bee0bece9157a9",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/js/2.e0f13d00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96e1b4f39e37e059a10f",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/js/main.2ed08a99.chunk.js"
+  },
+  {
+    "revision": "ccf4c40fb37acb1d1d29a3b8d0c6970d",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/js/pdf.worker.entry.0d620650.worker.js"
+  },
+  {
+    "revision": "ac3df35bcd7a2aa579e062210ce2d521",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/js/pdf.worker.entry.0d620650.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "950adacdbabf0965a01a",
+    "url": "https://rdok.github.io/lean-pdf-editor/static/js/runtime-main.6cb0c5c0.js"
+  }
+]);
