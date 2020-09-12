6119c6553265c9f4e59d5a00a7bae9ad5d1abed7
Rizart Dokollari <r.dokollari@gmail.com>
2020-09-12 13:04:31 +0100

    Setup foundation for modifying & exporting a pdf with simple text.

--- src/components/editor/Editor.js
+++ src/components/editor/Editor.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 
 import './Editor.scss';
+import ModifyPDF from "../modify-page";
+import downloadjs from "downloadjs";
 
 export default class Editor extends Component {
+
+  async handleDownload(e) {
+    const pdf = await ModifyPDF();
+    downloadjs(pdf, "pdf-lib_creation_example.pdf", "application/pdf");
+  }
 
   render() {
     return (
       <div className="ActionsForm">
         <div className="ActionsForm__container">
-          <form>
-            <input type="submit" value="Bookmark"/>
-          </form>
+          <button onClick={this.handleDownload}>Download</button>
         </div>
       </div>
     );
---
+++ src/components/modify-page.js
@@ -0,0 +1,27 @@
+import { degrees, PDFDocument, rgb, StandardFonts } from 'pdf-lib';
+
+const ModifyPDF = async () => {
+    const url = '/sample.pdf';
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer());
+
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
+    const { width, height } = firstPage.getSize();
+    firstPage.drawText('This text was added with JavaScript!', {
+      x: 5,
+      y: height / 2 + 300,
+      size: 50,
+      font: helveticaFont,
+      color: rgb(0.95, 0.1, 0.1),
+      rotate: degrees(-45),
+    });
+
+    const pdfBytes = await pdfDoc.save();
+
+    return pdfBytes
+}
+
+export default  ModifyPDF
