24ab26b6f7171ec5df2e5818d66d85a2f25a335e
GitHub Action <action@github.com>
2020-09-12 08:27:30 +0000

    Publish build 9754e7b66608a234d786c81091a6f54d2f798758

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.858458770c32b53e8491f302a1b1aeed.js"
+  "https://rdok.github.io/lean-pdf-editor/precache-manifest.2868381d0add765dff6d3a0d7be8b5a0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://rdok.github.io/lean-pdf-editor/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
